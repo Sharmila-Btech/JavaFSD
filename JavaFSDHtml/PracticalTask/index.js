1a4c03ae25f664ba697ae02bd949f9ecbf95008e
SHARMILA <sharmilarajkumar04@gmail.com>
2026-06-20 22:16:35 +0530

    Add files via upload

---
+++ JavaFSDHtml/PracticalTask/index.js
@@ -0,0 +1,27 @@
+// 1. Write a JavaScript program to: Declare variables using let and const  Perform addition, 
+// subtraction, multiplication, and division
+let num1 = 20;
+let num2 = 5;
+const message = "Arithmetic Operations";
+console.log(message);
+let addition = num1 + num2;
+console.log("Addition:", addition);
+let subtraction = num1 - num2;
+console.log("Subtraction:", subtraction);
+let multiplication = num1 * num2;
+console.log("Multiplication:", multiplication);
+let division = num1 / num2;
+console.log("Division:", division);
+
+
+// 2. Write a program to: Check whether a number is positive, negative, or zero
+console.log("...program 2...");
+
+let num = -10;
+if (num > 0) {
+    console.log("The number is Positive.");
+} else if (num < 0) {
+    console.log("The number is Negative.");
+} else {
+    console.log("The number is Zero.");
+}
